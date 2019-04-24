ea6169cbc7fd561ed4570d97557a12f4beb1107b
Jose Enciso <josenciso@zentcode.com>
2019-04-24 10:12:15 -0400

    fix informes permiso

--- Modules/Informes/Events/Handlers/RegisterInformesSidebar.php
+++ Modules/Informes/Events/Handlers/RegisterInformesSidebar.php
@@ -42,7 +42,7 @@ class RegisterInformesSidebar implements \Maatwebsite\Sidebar\SidebarExtender
                 $item->weight(11);
                 $item->route('admin.informes.informe.index');
                 $item->authorize(
-                   $this->auth->hasAccess('admin.informes.informe.index')
+                   $this->auth->hasAccess('informes.informes.index')
                 );
 // append
             });
