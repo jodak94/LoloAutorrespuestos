a4fc2f9d06e5cdbfa32d8df5bff8839e75b2c355
Jose Enciso <josenciso@zentcode.com>
2019-05-24 13:34:47 -0400

    diseño pdf

--- Modules/Presupuestos/Resources/views/pdf/partials/presupuesto-datos.blade.php
+++ Modules/Presupuestos/Resources/views/pdf/partials/presupuesto-datos.blade.php
@@ -1,24 +1,27 @@
+<table cellspacing="0" width="80%" style="margin:auto;border-collapse: collapse">
+  <tr style="border-left: 0;border-right: 0 ;border-bottom: 1px;border-top: 0; border-style: solid">
+       <td width="30%">
+           @if ($format == "html")
+               <img src="{{ url('images/logo.png') }}" width="150px" style="vertical-align: unset">
+           @else
+               <img src="{{ public_path('images/logo.png') }}" width="150px; vertical-align: top">
+           @endif
+        </td>
+        <td width="40%" style="text-align: center">
+           <div style="display:inline-block;font-size:12px;">
+               <p style="margin-bottom: 0;text-align: center; font-weight: 600; font-size: 17px">Lolo Autorepuestos SRL</p>
+               <p style="margin-bottom: 0;">Avda. Defensores Chaco c/ Cerro Corá</p>
+               <p>(021) 505 985</p>
+           </div>
+       </td>
+       <td width="30%" style="text-align: right">
+           <p style="margin-bottom: 0;"><b>Presupuesto</b></p>
+           <p><b>N° {{ $datosPresupuesto["nro_presupuesto"] }}</b></p>
+           <br>
+       </td>
+   </tr>
+</table>
 <table  cellspacing="0" width="80%" style="margin:auto;border-collapse: collapse" >
-       <tr style="border-left: 0;border-right: 0 ;border-bottom: 1px;border-top: 0; border-style: solid">
-            <td colspan="4">
-                @if ($format == "html")
-                    <img src="{{ url('images/logo.png') }}" width="150px" style="width: 30%; vertical-align: unset">
-                @else
-                    <img src="{{ public_path('images/logo.png') }}" width="150px">
-                @endif
-
-                <div style="padding-left:5%;display:inline-block;font-size:12px; width: 69%">
-                    <p style="margin-top:2%; text-align: center; font-weight: 600; font-size: 17px">Lolo Autorepuestos SRL</p>
-                    <p style="margin-bottom: 0;">Avda. Defensores Chaco c/ Cerro Corá</p>
-                    <p>(021) 505 985</p>
-                </div>
-            </td>
-            <td style="text-align: left">
-                <p><b>Presupuesto</b></p>
-                <p><b>N° {{ $datosPresupuesto["nro_presupuesto"] }}</b></p>
-                <br>
-            </td>
-        </tr>
         <tr>
             <td>
                 <p><b>Nombre del Cliente: </b> {{$datosPresupuesto["nombre_cliente"]}} </p>
@@ -29,16 +32,16 @@
                     <p><b>E-mail: </b> {{$datosPresupuesto["email_cliente"]}}</p>
             </td>
             <td colspan="2">
-              <p style="text-align: center;"><b>Fecha:</b> {{$datosPresupuesto["fecha"]}}</p>
+              <p style="text-align: right"><b>Fecha:</b> {{$datosPresupuesto["fecha"]}}</p>
               <p style="color: #fff">.</p>
             </td>
         </tr>
         <tr style="text-align: center;font-weight: bold">
-            <td style="border: 1px solid black">Concepto</td>
-            <td style="width: 4 %;border: 1px solid black">Cantidad</td>
-            <td style="width: 16%;border: 1px solid black">Precio Unitario</td>
-            <td style="width: 16%;border: 1px solid black">Descuento</td>
-            <td style="width: 16%;border: 1px solid black">Precio Total</td>
+            <td width="40%" style="border: 1px solid black">Concepto</td>
+            <td width="15%" style="border: 1px solid black">Cantidad</td>
+            <td width="15%" style="border: 1px solid black">Precio Unitario</td>
+            <td width="15%" style="border: 1px solid black">Descuento</td>
+            <td width="15%" style="border: 1px solid black">Precio Total</td>
         </tr>
         <tr style="border: 1px solid black">
             <td style="border: 1px solid black">
