9988637bcca2c9745e2b117735c829f7e0c9cd4a
Jose <josedacak@gmail.com>
2019-08-20 10:34:07 -0400

    ventas parciales pagados (fix)

--- Modules/Ventas/Http/Controllers/Admin/VentaController.php
+++ Modules/Ventas/Http/Controllers/Admin/VentaController.php
@@ -163,10 +163,9 @@ class VentaController extends AdminBaseController
           $query->where('tipo_factura', 'credito');
 
         if($re->has('parcial') && $re->parcial){
+          $query->where('parcial', true);
           if($re->has('parcial_pagado') && $re->parcial_pagado)
             $query->where('monto_pagado', '>=', DB::raw('monto_total'));
-          else
-            $query->where('parcial', true);
         }
 
         if($re->has('parcial') && !$re->parcial)
